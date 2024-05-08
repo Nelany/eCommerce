e5f228be1c33d4a74eee03dbfc4d41bd3eb62c0a
Nelany <svetasweetka@gmail.com>
2024-05-08 10:49:06 +0200

    feat: add MUI

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
 import './Header.scss';
 import NavItem from '../navItem/NavItem';
+import Button from '@mui/material/Button';
+
+
 const nav = [
   { text: 'Home', imgSrc: '/src/assets/home2.png' },
   { text: 'Catalog', imgSrc: '/src/assets/book2.png' },
@@ -14,6 +17,13 @@ const Header = () => {
         <NavItem text={navItem.text} imgSrc={navItem.imgSrc} />
       ))}
       <div className="sign-container">
+        <Button
+          style={{ backgroundColor: '#091D9E' }}
+          variant="contained"
+        >
+          Contained
+        </Button>
+
         <Link to={'/sign-in'}>Sign In</Link>
         <Link to={'/sign-up'}>Sign Up</Link>
       </div>
