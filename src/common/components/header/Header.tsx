9ca91e40ee4a891eba24f7258d1d510b7ba6c566
Nelany <svetasweetka@gmail.com>
2024-05-08 13:08:34 +0200

    fix: update header

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -4,10 +4,10 @@ import NavItem from '../navItem/NavItem';
 import Button from '@mui/material/Button';
 
 const nav = [
-  { text: 'Home', imgSrc: '/src/assets/home2.png' },
-  { text: 'Catalog', imgSrc: '/src/assets/book2.png' },
-  { text: 'Cart', imgSrc: '/src/assets/cart.png' },
-  { text: 'Profile', imgSrc: '/src/assets/hindu2.png' },
+  { text: 'Home', imgSrc: '/home2.png' },
+  { text: 'Catalog', imgSrc: '/book2.png' },
+  { text: 'Cart', imgSrc: '/cart.png' },
+  { text: 'Profile', imgSrc: '/hindu2.png' },
 ];
 
 const to = { SIGN_IN: '/sign-in', SIGN_UP: '/sign-up' };
@@ -15,25 +15,19 @@ const to = { SIGN_IN: '/sign-in', SIGN_UP: '/sign-up' };
 const Header = () => {
   const navigate = useNavigate();
   const onClick = (to: string) => {
-    navigate(to, { replace: true });
+    navigate(to);
   };
   return (
     <header className="nav">
-      {nav.map((navItem) => (
-        <NavItem text={navItem.text} imgSrc={navItem.imgSrc} />
+      {nav.map((navItem, index) => (
+        <NavItem text={navItem.text} imgSrc={navItem.imgSrc} key={index} />
       ))}
       <div className="sign-container">
-        <Button
-          variant="contained"
-          onClick={() => onClick(to.SIGN_IN)}
-        >
+        <Button variant="contained" onClick={() => onClick(to.SIGN_IN)}>
           Sign In
         </Button>
 
-        <Button
-          variant="contained"
-          onClick={() => onClick(to.SIGN_UP)}
-        >
+        <Button variant="contained" onClick={() => onClick(to.SIGN_UP)}>
           Sign Up
         </Button>
       </div>
