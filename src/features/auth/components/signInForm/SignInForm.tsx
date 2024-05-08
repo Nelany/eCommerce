563153febb72a73a098b3578c22b6e5e6acdb74b
Volha Patciahevich <zaprudskaya@gmail.com>
2024-05-08 22:44:21 +0300

    feat: create login form

--- src/features/auth/components/signInForm/SignInForm.tsx
+++ src/features/auth/components/signInForm/SignInForm.tsx
@@ -1,27 +1,48 @@
 // import { useNavigateToMain } from '../../../../common/hooks/useNavigateToMain';
-import Button from '../button/Button';
-import Input from '../input/Input';
-import './SignInForm.scss';
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
+import { styled } from '@mui/system';
 
+const PopUp = styled('main')({
+  width: 400,
+  height: 'max-content',
+
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 10,
+
+  padding: 8,
+  borderRadius: 4,
+})
+
+const LoginForm = styled('form')({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 'inherit'
+})
+
+const LoginTitle = styled('h2')({
+  fontSize: 24,
+})
 const SignInForm = () => {
   // const navigateToMain = useNavigateToMain();
-  const onClick = (event: Event) => {
-    event?.preventDefault()
+  const checkForm = () => {
     console.log('click');
     // тут проверка, после которой:
     // navigateToMain();
   };
- 
+
   return (
-    <div className='pop-up'>
-      <h2>Login</h2>
-      <form className='auth-form' name='login-form'>
-        <Input type='text' name='e-mail' error='e-mail error'/>
-        <Input type='password' name='password' error='password error'/>
-        <Button type='submit' text='Sign In' callback={onClick}/>
-      </form>
-    </div>
-  );
+    <PopUp>
+      <LoginTitle>Sign in </LoginTitle>
+       <LoginForm>
+        <TextField autoFocus id='e-mail' label='E-mail:' type='text'/>
+        <TextField  id='password' label='Password:' type='password'/>
+        <Button type='button' variant="contained" onClick={checkForm}> Sign In </Button>
+      </LoginForm>
+    </PopUp>
+  )
+
 };
 
 export default SignInForm;
