73d0ba4f25dc3b882e33d3671af1701f6ff139bb
Volha Paciahievič <70814944+patciahevich@users.noreply.github.com>
2024-05-09 12:17:05 +0300

    Ecomm 2 24 create login form (#22)
    * feat: add button and input components and create an example of the form
    
    * feat: create login form
    
    * fix: turn off autocomplete
    
    * feat: create login form using useForm React Hook
    
    * fix: work with review comments

--- src/features/auth/components/signInForm/SignInForm.tsx
+++ src/features/auth/components/signInForm/SignInForm.tsx
@@ -1,18 +1,60 @@
-import { useNavigateToMain } from '../../../../common/hooks/useNavigateToMain';
+// import { useNavigateToMain } from '../../../../common/hooks/useNavigateToMain';
+import Button from '@mui/material/Button';
+import { useForm, SubmitHandler } from "react-hook-form";
 import './SignInForm.scss';
 
+type LoginFormTypes = {
+  email: string;
+  password: string;
+};
+
+
+
 const SignInForm = () => {
-  const navigateToMain = useNavigateToMain();
-  const onClick = () => {
-    // тут проверка, после которой:
-    navigateToMain();
-  };
+  // const navigateToMain = useNavigateToMain();
+
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+    reset
+  } = useForm<LoginFormTypes>({mode: 'onChange'})
+
+  const onSubmit: SubmitHandler<LoginFormTypes> = data => {
+     // тут проверка, после которой:
+     // navigateToMain();
+    console.log(data);
+    reset();
+  }
+
   return (
-    <div>
-      SignInForm
-      <button onClick={onClick}>Sign In</button>
+    <div className='pop-up'>
+      <h2 className='login-title'>Login</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className='login-form'>
+        <input type='text' placeholder='E-mail' className='input' autoComplete='off' {
+          ...register('email', {
+            required: 'Please, enter your e-mail!'
+          })
+        }/>
+
+        {errors?.email && (
+          <div className='error'>{errors.email.message}</div>
+        )}
+
+        <input  type='password' placeholder='Password' className='input' {
+          ...register('password', {
+            required: 'Please, enter your password!'
+          })
+        }/>
+
+        {errors?.password && (
+          <div className='error'>{errors.password.message}</div>
+        )}
+
+        <Button type='submit' variant="contained"> Sign In </Button>
+      </form>
     </div>
-  );
+  )
 };
 
 export default SignInForm;
