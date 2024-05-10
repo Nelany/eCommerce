2d24895e8f28e1115a96c63a1c99bd2e85e0cf17
Oksana Bondareva <oksana20-89@mail.ru>
2024-05-10 20:29:32 +0300

    feat: add register form validation

--- src/features/auth/components/signUpForm/SignUpForm.tsx
+++ src/features/auth/components/signUpForm/SignUpForm.tsx
@@ -5,7 +5,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { registerDate } from '../../types/app.interface';
 
 const SignUpForm = () => {
-  const { register, handleSubmit, reset } = useForm<registerDate>();
+  const { register, handleSubmit, reset, formState: {errors} } = useForm<registerDate>();
   const navigateToMain = useNavigateToMain();
   const onSubmit: SubmitHandler<registerDate> = () => {
     reset();
@@ -18,80 +18,123 @@ const SignUpForm = () => {
       <form className={'form-register'} onSubmit={handleSubmit(onSubmit)}>
         <input
           className={'form-register__input'}
-          {...(register('name'),
+          {...register('name',
           {
-            required: true,
+            required: "Please, enter your name",
+            minLength: 1,
+            pattern: {
+              value: /[a-zA-Z]/,
+              message: "Your name must contain at least one character and no special characters or numbers"
+            }
           })}
           placeholder="Name"
           type="text"
-        ></input>
+        />
+        {errors?.name && (
+          <span className="error-validation">{errors.name.message}</span>
+        )}
         <input
           className={'form-register__input'}
-          {...(register('lastName'),
+          {...register('lastName',
           {
-            required: true,
+            required: "Please, enter your last name",
+            minLength: 1,
+            pattern: {
+              value: /[a-zA-Z]/,
+              message: "Your last name must contain at least one character and no special characters or numbers"
+            }
           })}
           placeholder="Last Name"
           type="text"
-        ></input>
+        />
+        {errors?.lastName && (
+          <span className="error-validation">{errors.lastName.message}</span>
+        )}
         <input
           className={'form-register__input'}
-          {...(register('email'),
+          {...register('email',
           {
-            required: true,
+            required: "Please, enter your email",
+            pattern: {
+              value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              message: "Enter your email in the format example@email.com"
+            }
           })}
           placeholder="Email"
-          type="email"
-        ></input>
+          type="text"
+        />
+        {errors?.email && (
+          <span className="error-validation">{errors.email.message}</span>
+        )}
         <input
           className={'form-register__input'}
-          {...(register('password'),
+          {...register('password',
           {
-            required: true,
+            required: "Please, enter your password",
+            pattern: {
+              value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+              message: "Password must contain minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, and 1 number"
+            }
           })}
           placeholder="Password"
           type="password"
-        ></input>
+        />
+        {errors.password && (
+          <span className="error-validation">{errors.password.message}</span>
+        )}
         <input
           className={'form-register__input'}
-          {...(register('dateBirth'),
+          {...register('dateBirth',
           {
             required: true,
+            max: "2006-05-10"
           })}
           placeholder="Date of Birth"
           type="date"
-        ></input>
+        />
+        {errors.dateBirth && (
+          <span className="error-validation">{`The age must be over 18 years old`}</span>
+        )}
         <select className={'form-register__input'}>
           <option value="GB">United Kingdom</option>
           <option value="US">United States</option>
         </select>
         <input
           className={'form-register__input'}
-          {...(register('address.city'),
+          {...register('address.city',
           {
-            required: true,
+            required: "Please, enter your city",
+            minLength: 1,
+            pattern: {
+              value: /[a-zA-Z]/,
+              message: "City must contain at least one character and no special characters or numbers"
+            }
           })}
           placeholder="City"
           type="text"
-        ></input>
+        />
+        {errors.address?.city && (
+          <span className="error-validation">{errors.address?.city.message}</span>
+        )}
         <input
           className={'form-register__input'}
-          {...(register('address.street'),
+          {...register('address.street',
           {
-            required: true,
+            required: "Please, enter your street",
+            minLength: 1,
           })}
           placeholder="Street"
           type="text"
-        ></input>
+        />
         <input
           className={'form-register__input'}
-          {...(register('address.postalCode'),
+          {...register('address.postalCode',
           {
-            required: true,
+            required: "Please, enter your postal code",
           })}
           placeholder="Postal Code"
           type="text"
-        ></input>
+        />
         <Button variant="contained" type="submit" fullWidth>
           Sign Up
         </Button>
