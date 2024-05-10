64b0bc1c6dd98533927ffea29bc92c9dbc4e7dca
Nelany <svetasweetka@gmail.com>
2024-05-10 22:00:15 +0200

    feat: add redirect user to the main page upon login

--- src/features/auth/components/signInForm/SignInForm.tsx
+++ src/features/auth/components/signInForm/SignInForm.tsx
@@ -1,6 +1,6 @@
 // import { useNavigateToMain } from '../../../../common/hooks/useNavigateToMain';
 import Button from '@mui/material/Button';
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler } from 'react-hook-form';
 import './SignInForm.scss';
 
 type LoginFormTypes = {
@@ -8,53 +8,59 @@ type LoginFormTypes = {
   password: string;
 };
 
-
-
 const SignInForm = () => {
   // const navigateToMain = useNavigateToMain();
 
   const {
     register,
     handleSubmit,
-    formState: {errors},
-    reset
-  } = useForm<LoginFormTypes>({mode: 'onChange'})
+    formState: { errors },
+    reset,
+  } = useForm<LoginFormTypes>({ mode: 'onChange' });
 
-  const onSubmit: SubmitHandler<LoginFormTypes> = data => {
-     // тут проверка, после которой:
-     // navigateToMain();
+  const onSubmit: SubmitHandler<LoginFormTypes> = (data) => {
+    // тут проверка, после которой:
+    // navigateToMain();
     console.log(data);
     reset();
-  }
+  };
 
   return (
-    <div className='pop-up'>
-      <h2 className='login-title'>Login</h2>
-      <form onSubmit={handleSubmit(onSubmit)} className='login-form'>
-        <input type='text' placeholder='E-mail' className='input' autoComplete='off' {
-          ...register('email', {
-            required: 'Please, enter your e-mail!'
-          })
-        }/>
+    <div className="pop-up">
+      <h2 className="login-title">Login</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className="login-form">
+        <input
+          type="text"
+          placeholder="E-mail"
+          className="input"
+          autoComplete="off"
+          {...register('email', {
+            required: 'Please, enter your e-mail!',
+          })}
+        />
 
-        {errors?.email && (
-          <div className='error'>{errors.email.message}</div>
-        )}
+        {errors?.email && <div className="error">{errors.email.message}</div>}
 
-        <input  type='password' placeholder='Password' className='input' {
-          ...register('password', {
-            required: 'Please, enter your password!'
-          })
-        }/>
+        <input
+          type="password"
+          placeholder="Password"
+          className="input"
+          {...register('password', {
+            required: 'Please, enter your password!',
+          })}
+        />
 
         {errors?.password && (
-          <div className='error'>{errors.password.message}</div>
+          <div className="error">{errors.password.message}</div>
         )}
 
-        <Button type='submit' variant="contained"> Sign In </Button>
+        <Button type="submit" variant="contained">
+          {' '}
+          Sign In{' '}
+        </Button>
       </form>
     </div>
-  )
+  );
 };
 
 export default SignInForm;
--- src/features/main/pages/Main.tsx
+++ src/features/main/pages/Main.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react';
 import './Main.scss';
 import { getCustomers } from '../../auth/api/auth';
 import useSelectUser from '../../auth/hooks/useSelectUser';
 import useDispatchUserId from '../../auth/hooks/useDispatchUserId';
 
 function App() {
-  const [count, setCount] = useState(0);
   const userId = useSelectUser();
   const setUser = useDispatchUserId();
 
-  const onClick = () => {
+  const onClick = (userId: string) => {
     // сохраняет пользователя в локалсторэдж и редаксстор:
-    setUser('Coolguy' + Math.random());
+    setUser(userId);
     // пример получения данных пользователя Джона:
     getCustomers('johndoe@example.com').then(console.log).catch(console.error);
     // пример создания пользователя:
@@ -33,12 +31,19 @@ function App() {
     <>
       <h1>COOLSTORE</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          onClick={
+            () => onClick('')
+            // еще очистить стор и локалсторэдж от других возможных данных
+          }
+        >
           {userId}
+          (LOG OUT)
         </button>
       </div>
-      <button onClick={onClick}>click me</button>
+      <button onClick={() => onClick(`Collguy ${Math.random()}`)}>
+        create User In Localstorage!!! (LOG IN)
+      </button>
     </>
   );
 }
