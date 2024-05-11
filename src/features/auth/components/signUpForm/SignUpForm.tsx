a79d00c27af1571c54783992678ab9f1823bd18f
Oksana Bondareva <oksana20-89@mail.ru>
2024-05-12 00:54:42 +0300

    feat: add validation postal code

--- src/features/auth/components/signUpForm/SignUpForm.tsx
+++ src/features/auth/components/signUpForm/SignUpForm.tsx
@@ -5,12 +5,39 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { registerDate } from '../../types/app.interface';
 
 const SignUpForm = () => {
-  const { register, handleSubmit, reset, formState: {errors} } = useForm<registerDate>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<registerDate>();
   const navigateToMain = useNavigateToMain();
   const onSubmit: SubmitHandler<registerDate> = () => {
     reset();
     navigateToMain();
   };
+
+  function updateInputPattern() {
+    const select = document.getElementById(
+      'countrySelect'
+    ) as HTMLSelectElement;
+    const postalCodeInput = document.getElementById(
+      'postalCodeInput'
+    ) as HTMLInputElement;
+
+    if (select?.value === 'GB') {
+      postalCodeInput.pattern =
+        '^[A-Z]{1,2}[0-9RCHNQ][0-9A-Z]?s?[0-9][ABD-HJLNP-UW-Z]{2}$|^[A-Z]{2}-?[0-9]{4}$';
+      postalCodeInput.title =
+        'Please, enter your postal code, for example B294HJ';
+      postalCodeInput.disabled = false;
+    } else if (select.value === 'US') {
+      postalCodeInput.pattern = '^[0-9]{5}(-[0-9]{4})?$';
+      postalCodeInput.title =
+        'Please, enter your postal code, for example 32344-4444';
+      postalCodeInput.disabled = false;
+    }
+  }
 
   return (
     <div>
@@ -18,14 +45,14 @@ const SignUpForm = () => {
       <form className={'form-register'} onSubmit={handleSubmit(onSubmit)}>
         <input
           className={'form-register__input'}
-          {...register('name',
-          {
-            required: "Please, enter your name",
+          {...register('name', {
+            required: 'Please, enter your name',
             minLength: 1,
             pattern: {
               value: /[a-zA-Z]/,
-              message: "Your name must contain at least one character and no special characters or numbers"
-            }
+              message:
+                'Your name must contain at least one character and no special characters or numbers',
+            },
           })}
           placeholder="Name"
           type="text"
@@ -35,14 +62,14 @@ const SignUpForm = () => {
         )}
         <input
           className={'form-register__input'}
-          {...register('lastName',
-          {
-            required: "Please, enter your last name",
+          {...register('lastName', {
+            required: 'Please, enter your last name',
             minLength: 1,
             pattern: {
               value: /[a-zA-Z]/,
-              message: "Your last name must contain at least one character and no special characters or numbers"
-            }
+              message:
+                'Your last name must contain at least one character and no special characters or numbers',
+            },
           })}
           placeholder="Last Name"
           type="text"
@@ -52,13 +79,12 @@ const SignUpForm = () => {
         )}
         <input
           className={'form-register__input'}
-          {...register('email',
-          {
-            required: "Please, enter your email",
+          {...register('email', {
+            required: 'Please, enter your email',
             pattern: {
               value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
-              message: "Enter your email in the format example@email.com"
-            }
+              message: 'Enter your email in the format example@email.com',
+            },
           })}
           placeholder="Email"
           type="text"
@@ -68,13 +94,13 @@ const SignUpForm = () => {
         )}
         <input
           className={'form-register__input'}
-          {...register('password',
-          {
-            required: "Please, enter your password",
+          {...register('password', {
+            required: 'Please, enter your password',
             pattern: {
               value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
-              message: "Password must contain minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, and 1 number"
-            }
+              message:
+                'Password must contain minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, and 1 number',
+            },
           })}
           placeholder="Password"
           type="password"
@@ -84,10 +110,9 @@ const SignUpForm = () => {
         )}
         <input
           className={'form-register__input'}
-          {...register('dateBirth',
-          {
+          {...register('dateBirth', {
             required: true,
-            max: "2006-05-10"
+            max: '2006-05-10',
           })}
           placeholder="Date of Birth"
           type="date"
@@ -95,46 +120,65 @@ const SignUpForm = () => {
         {errors.dateBirth && (
           <span className="error-validation">{`The age must be over 18 years old`}</span>
         )}
-        <select className={'form-register__input'}>
+        <select
+          className={'form-register__input'}
+          id="countrySelect"
+          onChange={updateInputPattern}
+        >
+          <option selected={true} disabled={true}>
+            Select country
+          </option>
           <option value="GB">United Kingdom</option>
           <option value="US">United States</option>
         </select>
         <input
           className={'form-register__input'}
-          {...register('address.city',
-          {
-            required: "Please, enter your city",
+          {...register('address.city', {
+            required: 'Please, enter your city',
             minLength: 1,
             pattern: {
               value: /[a-zA-Z]/,
-              message: "City must contain at least one character and no special characters or numbers"
-            }
+              message:
+                'City must contain at least one character and no special characters or numbers',
+            },
           })}
           placeholder="City"
           type="text"
         />
         {errors.address?.city && (
-          <span className="error-validation">{errors.address?.city.message}</span>
+          <span className="error-validation">
+            {errors.address?.city.message}
+          </span>
         )}
         <input
           className={'form-register__input'}
-          {...register('address.street',
-          {
-            required: "Please, enter your street",
+          {...register('address.street', {
+            required: 'Please, enter your street',
             minLength: 1,
           })}
           placeholder="Street"
           type="text"
         />
+        {errors.address?.street && (
+          <span className="error-validation">{`Please, enter your street`}</span>
+        )}
         <input
           className={'form-register__input'}
-          {...register('address.postalCode',
-          {
-            required: "Please, enter your postal code",
+          id="postalCodeInput"
+          {...register('address.postalCode', {
+            required: true,
           })}
           placeholder="Postal Code"
           type="text"
+          disabled
         />
+        {errors.address?.postalCode && (
+          <span className="error-validation">
+            {
+              'Please, enter your postal code, for example B294HJ for United Kingdom or 32344-4444 for United States'
+            }
+          </span>
+        )}
         <Button variant="contained" type="submit" fullWidth>
           Sign Up
         </Button>
