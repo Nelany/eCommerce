c3bb9611e9b87e534fc9f32c692575cfafea4a52
Oksana Bondareva <oksana20-89@mail.ru>
2024-05-13 21:00:55 +0300

    feat: add billing address in form

--- src/features/auth/components/signUpForm/SignUpForm.tsx
+++ src/features/auth/components/signUpForm/SignUpForm.tsx
@@ -25,24 +25,41 @@ const SignUpForm = () => {
   }, []);
 
   function updateInputPattern() {
-    const select = document.getElementById(
-      'countrySelect'
+    const selectShipping = document.getElementById(
+      'countrySelectShipping'
     ) as HTMLSelectElement;
-    const postalCodeInput = document.getElementById(
-      'postalCodeInput'
+    const postalCodeInputShipping = document.getElementById(
+      'postalCodeInputShipping'
+    ) as HTMLInputElement;
+    const selectBilling = document.getElementById(
+      'countrySelectBilling'
+    ) as HTMLSelectElement;
+    const postalCodeInputBilling = document.getElementById(
+      'postalCodeInputBilling'
     ) as HTMLInputElement;
 
-    if (select?.value === 'GB') {
-      postalCodeInput.pattern =
+    if (selectShipping.value === 'GB') {
+      postalCodeInputShipping.pattern =
         '^[A-Z]{1,2}[0-9RCHNQ][0-9A-Z]?s?[0-9][ABD-HJLNP-UW-Z]{2}$|^[A-Z]{2}-?[0-9]{4}$';
-      postalCodeInput.title =
+      postalCodeInputShipping.title =
         'Please, enter your postal code, for example B294HJ';
-      postalCodeInput.disabled = false;
-    } else if (select.value === 'US') {
-      postalCodeInput.pattern = '^[0-9]{5}(-[0-9]{4})?$';
-      postalCodeInput.title =
+      postalCodeInputShipping.disabled = false;
+    } else if (selectShipping.value === 'US') {
+      postalCodeInputShipping.pattern = '^[0-9]{5}(-[0-9]{4})?$';
+      postalCodeInputShipping.title =
         'Please, enter your postal code, for example 32344-4444';
-      postalCodeInput.disabled = false;
+      postalCodeInputShipping.disabled = false;
+    } else if (selectBilling.value === 'GB') {
+      postalCodeInputBilling.pattern =
+        '^[A-Z]{1,2}[0-9RCHNQ][0-9A-Z]?s?[0-9][ABD-HJLNP-UW-Z]{2}$|^[A-Z]{2}-?[0-9]{4}$';
+      postalCodeInputBilling.title =
+        'Please, enter your postal code, for example B294HJ';
+      postalCodeInputBilling.disabled = false;
+    } else if (selectBilling.value === 'US') {
+      postalCodeInputBilling.pattern = '^[0-9]{5}(-[0-9]{4})?$';
+      postalCodeInputBilling.title =
+        'Please, enter your postal code, for example 32344-4444';
+      postalCodeInputBilling.disabled = false;
     }
   }
 
@@ -135,65 +152,147 @@ const SignUpForm = () => {
         {errors.dateBirth && (
           <span className="error-validation">{`The age must be over 18 years old`}</span>
         )}
-        <select
-          className={'form-register__input'}
-          id="countrySelect"
-          onChange={updateInputPattern}
-        >
-          <option selected={true} disabled={true}>
-            Select country
-          </option>
-          <option value="GB">United Kingdom</option>
-          <option value="US">United States</option>
-        </select>
-        <input
-          className={'form-register__input'}
-          {...register('address.city', {
-            required: 'Please, enter your city',
-            minLength: 1,
-            pattern: {
-              value: /[a-zA-Z]/,
-              message:
-                'City must contain at least one character and no special characters or numbers',
-            },
-          })}
-          placeholder="City"
-          type="text"
-        />
-        {errors.address?.city && (
-          <span className="error-validation">
-            {errors.address?.city.message}
-          </span>
-        )}
-        <input
-          className={'form-register__input'}
-          {...register('address.street', {
-            required: 'Please, enter your street',
-            minLength: 1,
-          })}
-          placeholder="Street"
-          type="text"
-        />
-        {errors.address?.street && (
-          <span className="error-validation">{`Please, enter your street`}</span>
-        )}
-        <input
-          className={'form-register__input'}
-          id="postalCodeInput"
-          {...register('address.postalCode', {
-            required: true,
-          })}
-          placeholder="Postal Code"
-          type="text"
-          disabled
-        />
-        {errors.address?.postalCode && (
-          <span className="error-validation">
-            {
-              'Please, enter your postal code, for example B294HJ for United Kingdom or 32344-4444 for United States'
-            }
-          </span>
-        )}
+        <div className={'address-wrapper'}>
+          <select
+            className={'form-register__input'}
+            id="countrySelectShipping"
+            onChange={updateInputPattern}
+          >
+            <option selected={true} disabled={true}>
+              Select country
+            </option>
+            <option value="GB">United Kingdom</option>
+            <option value="US">United States</option>
+          </select>
+          <div className={'address-input-wrapper'}>
+            <input
+              className={'form-register__input'}
+              {...register('addressShipping.cityShipping', {
+                required: 'Please, enter your city',
+                minLength: 1,
+                pattern: {
+                  value: /[a-zA-Z]/,
+                  message:
+                    'City must contain at least one character and no special characters or numbers',
+                },
+              })}
+              placeholder="City"
+              type="text"
+            />
+            {errors.addressShipping?.cityShipping && (
+              <span className="error-validation">
+                {errors.addressShipping?.cityShipping.message}
+              </span>
+            )}
+          </div>
+          <div className={'address-input-wrapper'}>
+            <input
+              className={'form-register__input'}
+              {...register('addressShipping.streetShipping', {
+                required: 'Please, enter your street',
+                minLength: 1,
+              })}
+              placeholder="Street"
+              type="text"
+            />
+            {errors.addressShipping?.streetShipping && (
+              <span className="error-validation">{`Please, enter your street`}</span>
+            )}
+          </div>
+          <div className={'address-input-wrapper'}>
+            <input
+              className={'form-register__input'}
+              id="postalCodeInputShipping"
+              {...register('addressShipping.postalCodeShipping', {
+                required: true,
+              })}
+              placeholder="Postal Code"
+              type="text"
+              disabled
+            />
+            {errors.addressShipping?.postalCodeShipping && (
+              <span className="error-validation">
+                {
+                  'Please, enter your postal code, for example B294HJ for United Kingdom or 32344-4444 for United States'
+                }
+              </span>
+            )}
+          </div>
+        </div>
+        <div className={'checkbox-input-wrapper'}>
+          <input type="checkbox" />
+          <span>{'Set as default address'}</span>
+        </div>
+        <h4>Billing address</h4>
+        <div className={'address-wrapper'}>
+          <select
+            className={'form-register__input'}
+            id="countrySelectBilling"
+            onChange={updateInputPattern}
+          >
+            <option selected={true} disabled={true}>
+              Select country
+            </option>
+            <option value="GB">United Kingdom</option>
+            <option value="US">United States</option>
+          </select>
+          <div className={'address-input-wrapper'}>
+            <input
+              className={'form-register__input'}
+              id='cityBilling'
+              {...register('addressBilling.cityBilling', {
+                required: 'Please, enter your city',
+                minLength: 1,
+                pattern: {
+                  value: /[a-zA-Z]/,
+                  message:
+                    'City must contain at least one character and no special characters or numbers',
+                },
+              })}
+              placeholder="City"
+              type="text"
+            />
+            {errors.addressBilling?.cityBilling && (
+              <span className="error-validation">
+                {errors.addressBilling?.cityBilling.message}
+              </span>
+            )}
+          </div>
+          <div className={'address-input-wrapper'}>
+            <input
+              className={'form-register__input'}
+              id='streetBilling'
+              {...register('addressBilling.streetBilling', {
+                required: 'Please, enter your street',
+                minLength: 1,
+              })}
+              placeholder="Street"
+              type="text"
+            />
+            {errors.addressBilling?.streetBilling && (
+              <span className="error-validation">{`Please, enter your street`}</span>
+            )}
+          </div>
+          <div className={'address-input-wrapper'}>
+            <input
+              className={'form-register__input'}
+              id="postalCodeInputBilling"
+              {...register('addressBilling.postalCodeBilling', {
+                required: true,
+              })}
+              placeholder="Postal Code"
+              type="text"
+              disabled
+            />
+            {errors.addressBilling?.postalCodeBilling && (
+              <span className="error-validation">
+                {
+                  'Please, enter your postal code, for example B294HJ for United Kingdom or 32344-4444 for United States'
+                }
+              </span>
+            )}
+          </div>
+        </div>
         <Button variant="contained" type="submit" fullWidth>
           Sign Up
         </Button>
--- src/features/auth/types/app.interface.ts
+++ src/features/auth/types/app.interface.ts
@@ -1,8 +1,15 @@
-export interface Address {
-    country: string;
-    city: string;
-    street: string;
-    postalCode: string;
+export interface AddressShipping {
+    countryShipping: string;
+    cityShipping: string;
+    streetShipping: string;
+    postalCodeShipping: string;
+}
+
+export interface AddressBilling {
+    countryBilling: string;
+    cityBilling: string;
+    streetBilling: string;
+    postalCodeBilling: string;
 }
 
 export interface registerData {
@@ -11,5 +18,6 @@ export interface registerData {
     email: string;
     password: string;
     dateBirth: string;
-    address: Address;
+    addressShipping: AddressShipping;
+    addressBilling: AddressBilling;
 }
