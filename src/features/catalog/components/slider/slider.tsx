168e459b916cf8cb60d1cae7bc7629ba598943a3
Volha Patciahevich <zaprudskaya@gmail.com>
2024-05-22 14:40:38 +0300

    refactor: use map to create slides

--- src/features/catalog/components/slider/slider.tsx
+++ src/features/catalog/components/slider/slider.tsx
@@ -2,11 +2,7 @@ import img1 from '../../../../assets/test-slider-images/3-1.jpg';
 import img2 from '../../../../assets/test-slider-images/3-2.jpg';
 import img3 from '../../../../assets/test-slider-images/3-3.jpg';
 
-// import Slider from 'react-slick';
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import './slider.scss';
 
@@ -22,17 +18,17 @@ export default function CardSlider() {
     transitionTime: 1000,
     interval: 5000,
   };
+
+  const slides = [img1, img2, img3];
   return (
     <Carousel {...settings} className="slider">
-      <div className="slider-item">
-        <img className="slider-image" src={img1} />
-      </div>
-      <div className="slider-item">
-        <img className="slider-image" src={img2} />
-      </div>
-      <div className="slider-item">
-        <img className="slider-image" src={img3} />
-      </div>
+      {slides.map((slide, slideIndex) => {
+        return (
+          <div className="slider-item" key={slideIndex}>
+            <img className="slider-image" src={slide} />
+          </div>
+        );
+      })}
     </Carousel>
   );
 }
