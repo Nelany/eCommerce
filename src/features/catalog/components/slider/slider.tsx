b12b154414283f611bd64c42f5adbb19742f0701
Volha Paciahievič <70814944+patciahevich@users.noreply.github.com>
2024-05-22 15:54:32 +0300

    Ecomm 3 23 create card slider (#60)
    * feat: add slider component
    
    * refactor: use map to create slides
    
    * chore: delete unnecessary packages
    
    * fix: fix slider position

---
+++ src/features/catalog/components/slider/slider.tsx
@@ -0,0 +1,34 @@
+import img1 from '../../../../assets/test-slider-images/3-1.jpg';
+import img2 from '../../../../assets/test-slider-images/3-2.jpg';
+import img3 from '../../../../assets/test-slider-images/3-3.jpg';
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+import './slider.scss';
+
+export default function CardSlider() {
+  const settings = {
+    showArrows: false,
+    infiniteLoop: true,
+    showThumbs: false,
+    showStatus: false,
+    autoPlay: true,
+    swipeable: true,
+    emulateTouch: true,
+    transitionTime: 1000,
+    interval: 5000,
+  };
+
+  const slides = [img1, img2, img3];
+  return (
+    <Carousel {...settings} className="slider">
+      {slides.map((slide, slideIndex) => {
+        return (
+          <div className="slider-item" key={slideIndex}>
+            <img className="slider-image" src={slide} />
+          </div>
+        );
+      })}
+    </Carousel>
+  );
+}
