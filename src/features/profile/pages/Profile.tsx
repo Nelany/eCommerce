2636f32180b5e0659bff594fb5310628d208eaf4
Oksana Bondareva <oksana20-89@mail.ru>
2024-05-28 02:06:11 +0300

    feat: add edit user details

--- src/features/profile/pages/Profile.tsx
+++ src/features/profile/pages/Profile.tsx
@@ -1,11 +1,19 @@
-import { Customer } from '@commercetools/platform-sdk';
+import {
+  Customer,
+  CustomerSetFirstNameAction,
+  CustomerSetLastNameAction,
+  CustomerSetDateOfBirthAction,
+} from '@commercetools/platform-sdk';
 import { auth } from '../../auth/api/auth';
 import './Profile.scss';
 import { useEffect, useState } from 'react';
 import useApi from '../../../common/hooks/useApi';
+import { getApiRoot } from '../../../common/api/sdk';
+import { Button } from '@mui/material';
+import useDispatchToast from '../../../common/hooks/useDispatchToast';
 
 const Profile = () => {
-  const key = localStorage.getItem('userId');
+  const key = localStorage.getItem('userId') as string;
   const [profile, setProfile] = useState<Customer | null>(null);
   const [defaultShippingAddress, setDefaultShippingAddress] =
     useState<string>('');
@@ -21,7 +29,6 @@ const Profile = () => {
       return profile;
     };
     getCustomer().then((profileData) => {
-      console.log(profileData);
       if (!profileData) return;
       const userProfile = profileData?.body.results[0];
       setProfile(userProfile);
@@ -60,78 +67,188 @@ const Profile = () => {
     });
   }, []);
 
-  if (!profile) {
-    return <h1>Profile Not Found</h1>;
-  }
+  const [editing, setEditing] = useState<boolean>(false);
+  const [firstName, setFirstName] = useState(profile?.firstName);
+  const [lastName, setLastName] = useState(profile?.lastName);
+  const [birthDate, setBirthDate] = useState(profile?.dateOfBirth);
+  const setToast = useDispatchToast();
+
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleNameChange = async (
+    newFirstName: string | undefined,
+    newLastName: string | undefined,
+    newBirthDate: string | undefined
+  ) => {
+    const customerVersion = profile?.version as number;
+    const changeNameAction: CustomerSetFirstNameAction = {
+      action: 'setFirstName',
+      firstName: newFirstName || profile?.firstName,
+    };
+
+    const changeLastNameAction: CustomerSetLastNameAction = {
+      action: 'setLastName',
+      lastName: newLastName || profile?.lastName,
+    };
+
+    const changeBirthDateAction: CustomerSetDateOfBirthAction = {
+      action: 'setDateOfBirth',
+      dateOfBirth: newBirthDate || profile?.dateOfBirth,
+    };
+
+    const response = await apiCall(
+      getApiRoot()
+        .customers()
+        .withId({ ID: key })
+        .post({
+          body: {
+            version: customerVersion,
+            actions: [
+              changeNameAction,
+              changeLastNameAction,
+              changeBirthDateAction,
+            ],
+          },
+        })
+        .execute()
+    );
+    setEditing(false);
+    if (response) {
+      setToast({
+        message: 'The data has been successfully updated',
+        type: 'success',
+        isToastOpen: true,
+      });
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleNameChange(firstName, lastName, birthDate);
+  };
+
+  const handleCancel = () => {
+    setEditing(false);
+  };
 
   return (
     <div className="page profile-page">
       <h1>PROFILE</h1>
-      <div className="profile-wrapper">
-        <div>
-          <h2>User info</h2>
-          <div className="profile-section">
-            <img
-              className="profile-icon"
-              src="src\assets\profile-page-icons\4092564-about-mobile-ui-profile-ui-user-website_114033.png"
-              alt="icon user"
-            ></img>
-            <span className="user-title">First name:</span>
-            <span>{profile?.firstName}</span>
-          </div>
-          <div className="profile-section">
-            <img
-              className="profile-icon"
-              src="src\assets\profile-page-icons\3643745-human-man-people-person-profile_113435.png"
-              alt="icon user"
-            ></img>
-            <span className="user-title">Last name:</span>
-            <span>{profile?.lastName}</span>
-          </div>
-          <div className="profile-section">
-            <img
-              className="profile-icon"
-              src="src\assets\profile-page-icons\calendar-check_icon-icons.com_56836.png"
-              alt="icon calendar"
-            ></img>
-            <span className="user-title">Date of birth:</span>
-            <span>{profile?.dateOfBirth}</span>
-          </div>
-        </div>
-        <div>
-          <h2>Addresses</h2>
+      {editing ? (
+        <form className="edit-form" onSubmit={handleSubmit}>
+          <label className="edit-form__label">
+            First name:
+            <input
+              className="edit-form__input"
+              type="text"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </label>
+          <label className="edit-form__label">
+            Last name:
+            <input
+              className="edit-form__input"
+              type="text"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </label>
+          <label className="edit-form__label">
+            Date of Birth:
+            <input
+              className="edit-form__input"
+              type="date"
+              onChange={(e) => setBirthDate(e.target.value)}
+            />
+          </label>
+          <Button className="edit-button" variant="contained" type="submit">
+            Save
+          </Button>
+          <Button
+            className="edit-button"
+            variant="contained"
+            onClick={handleCancel}
+          >
+            Cancel
+          </Button>
+        </form>
+      ) : (
+        <div className="profile-wrapper">
           <div>
+            <Button
+              variant="contained"
+              className="edit-button"
+              onClick={handleEditClick}
+            >
+              Edit profile ✎
+            </Button>
+          </div>
+          <div>
+            <h2>User info</h2>
             <div className="profile-section">
               <img
                 className="profile-icon"
-                src="src\assets\profile-page-icons\location_pin_place_map_address_placeholder_route_road_icon_149105.png"
-                alt="icon location"
+                src="/4092564-about-mobile-ui-profile-ui-user-website_114033.png"
+                alt="icon user"
               ></img>
-              <span className="user-title">Shipping address:</span>
-              <span>{shippingAddress}</span>
+              <span className="user-title">First name:</span>
+              <span>{firstName || profile?.firstName}</span>
             </div>
-            <div className="profile-section profile-section_address">
-              <span className="user-title">Default shipping address:</span>
-              <span>{defaultShippingAddress}</span>
-            </div>
-          </div>
-          <div>
             <div className="profile-section">
               <img
                 className="profile-icon"
-                src="src\assets\profile-page-icons\location_pin_place_map_address_placeholder_icon_149107.png"
-                alt="icon location"
+                src="/3643745-human-man-people-person-profile_113435.png"
+                alt="icon user"
               ></img>
-              <span className="user-title">Billing address:</span>
-              <span>{billingAddress}</span>
+              <span className="user-title">Last name:</span>
+              <span>{lastName || profile?.lastName}</span>
             </div>
             <div className="profile-section">
-              <span className="user-title">Default billing address:</span>
-              <span>{defaultBillingAddress}</span>
+              <img
+                className="profile-icon"
+                src="/calendar-check_icon-icons.com_56836.png"
+                alt="icon calendar"
+              ></img>
+              <span className="user-title">Date of birth:</span>
+              <span>{birthDate || profile?.dateOfBirth}</span>
+            </div>
+          </div>
+          <div>
+            <h2>Addresses</h2>
+            <div>
+              <div className="profile-section">
+                <img
+                  className="profile-icon"
+                  src="/location_pin_place_map_address_placeholder_route_road_icon_149105.png"
+                  alt="icon location"
+                ></img>
+                <span className="user-title">Shipping address:</span>
+                <span>{shippingAddress}</span>
+              </div>
+              <div className="profile-section profile-section_address">
+                <span className="user-title">Default shipping address:</span>
+                <span>{defaultShippingAddress}</span>
+              </div>
+            </div>
+            <div>
+              <div className="profile-section">
+                <img
+                  className="profile-icon"
+                  src="/location_pin_place_map_address_placeholder_icon_149107.png"
+                  alt="icon location"
+                ></img>
+                <span className="user-title">Billing address:</span>
+                <span>{billingAddress}</span>
+              </div>
+              <div className="profile-section">
+                <span className="user-title">Default billing address:</span>
+                <span>{defaultBillingAddress}</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
