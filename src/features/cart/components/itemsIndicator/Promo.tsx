993a4a3c50d761bef0932b71270e1f1c7972f25a
Nelany <svetasweetka@gmail.com>
2024-06-08 10:42:54 +0200

    feat: add promo-code

--- src/features/cart/components/itemsIndicator/Promo.tsx
+++ src/features/cart/components/itemsIndicator/Promo.tsx
@@ -2,13 +2,12 @@ import { Button } from '@mui/material';
 import useApi from '../../../../common/hooks/useApi';
 import { saveUserCart, updateCartById } from '../../../catalog/utils/helpers';
 import { useState } from 'react';
+import './Promo.scss';
 
 export const Promo = () => {
   const apiCall = useApi();
-
   const storedCartData = localStorage.getItem('cartData');
-
-  const discountCode = 'promoForBestStudents';
+  const [inputValue, setInputValue] = useState('');
   const [isDiscounted, setIsDiscounted] = useState(false);
   // При получении корзины с сервера нужно добавить проверку, применен ли уже дисконт для корзины:
   // const discountResponse =
@@ -25,7 +24,7 @@ export const Promo = () => {
         id: cartData.cartId,
         version: cartData.cartVersion,
         apiCall,
-        discountCode,
+        discountCode: inputValue,
       });
 
       cartResponse.then((cartResponseData) => {
@@ -45,15 +44,27 @@ export const Promo = () => {
   }
 
   return (
-    <div>
-      <Button
-        disabled={isDiscounted}
-        className="header-button"
-        variant="contained"
-        onClick={clickPromo}
-      >
-        {isDiscounted ? 'ADDED!' : 'ADD PROMO'}
-      </Button>
-    </div>
+    <>
+      <h2 className="promo-h2">promoForBestStudents</h2>
+      <div className="promo">
+        <input
+          className="promo__input"
+          id="inputPROMO"
+          placeholder="Enter PROMO"
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+
+        <Button
+          disabled={isDiscounted}
+          className="promo__button"
+          variant="contained"
+          onClick={clickPromo}
+        >
+          {isDiscounted ? 'ADDED!' : 'ADD PROMO'}
+        </Button>
+      </div>
+    </>
   );
 };
