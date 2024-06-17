9b06fa5d94ca8dbfb4e24c1f3d3ee45ca5e63844
Nelany <svetasweetka@gmail.com>
2024-06-17 22:22:38 +0200

    fix: update cart version

--- src/features/auth/api/auth.ts
+++ src/features/auth/api/auth.ts
@@ -2,15 +2,8 @@ import { getApiRoot } from '../../../common/api/sdk';
 import { UserData } from '../../../common/types';
 import { AddressBilling, AddressShipping } from '../types/app.interface';
 
-// Example call to return Project information
-// This code has the same effect as sending a GET request to the commercetools Composable Commerce API without any endpoints.
-// export const getProject = () => {
-//   return getApiRoot().get().execute();
-// };
 const getCustomers = (key: string | null) => {
-  // пример итогового запроса:
-  // https://api.us-central1.gcp.commercetools.com/cool-coders/customers?where=email%3D%22johndoe%40example.com%22
-  return getApiRoot()
+ return getApiRoot()
     .customers()
     .get({ queryArgs: { where: `id="${key}"` } })
     .execute();
--- src/features/auth/components/signUpForm/SignUpForm.test.tsx
+++ src/features/auth/components/signUpForm/SignUpForm.test.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 
 import { store } from '../../../../common/store/store';
 import { MemoryRouter } from 'react-router-dom';
-// import 'jest-localstorage-mock';
 
 jest.mock('../../../../common/hooks/useApi', () => ({
   __esModule: true,
